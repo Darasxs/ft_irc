f6c02b5250b5caaed8411da9af186984dccc0bfe
Darasxs <dariuszpaluszkiewicz113@gmail.com>
2025-06-05 20:34:07 +0200

    extended the channel class in the header file

--- Channel.hpp
+++ Channel.hpp
@@ -12,6 +12,13 @@ class Channel
 	std::vector<Client> operators;
   public:
 	// some function for sending message in the channel?
+	Channel(void);
+	Channel(const std::string &name);
+	std::string getTopic(void) const;
+	void addClient(const Client &client);
+	void removeClient(const Client &client);
+	void addOperator(const Client &client);
+	void removeOperator(const Client &client);
 
 };
 
