6410d331ffd676d45a9d3a9c23d02d761880121b
Darasxs <dariuszpaluszkiewicz113@gmail.com>
2025-06-07 15:14:11 +0200

    Server class improved in the header file by adding function prototypes etc.

--- Server.hpp
+++ Server.hpp
@@ -4,14 +4,17 @@
 # include <iostream>
 # include <vector>
 # include <map>
+# include "Client.hpp"
+# include "Channel.hpp"
+
 class Server
 {
   private:
 	int portNumber;
 	int serverFd; // socket that listens for connections, binds to a specific port and accept conncections
 	std::string hostName;
-	std::vector<Client> clients;
-	std::map<std::string, Channel> channels;
+	std::vector<Client*> clients;
+	std::map<std::string, Channel*> channels;
 	bool isRunning;
 	std::string password;
 
@@ -21,6 +24,14 @@ class Server
 	Server(const Server &other);
 	Server &operator=(const Server &other);
 	~Server(void);
+
+	void addClient(Client *client);
+	void removeClient(Client *client);
+	Client* getClient(const std::string &nickname) const; // identifying client by nickname, for example to send private message to him
+
+	void createChannel(const std::string &channelName);
+	void deleteChannel(const std::string &channelName);
+	Channel* getChannel(const std::string &channelName) const; // similar as getClient
 
 	void	serverInitialization(void);
 	void	setPassword(const std::string &password);
